271fcd53b53db7788b230b9f0dc53206d0846bec
Urs Grupp <urs@21sieben.de>
2021-07-05 15:08:56 +0200

    chore: update npm packages, remove vite imagemin plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,6 @@
 import legacy from "@vitejs/plugin-legacy";
-import path from "path";
 import { ConfigEnv, UserConfig } from "vite";
 import FullReload from "vite-plugin-full-reload";
-import viteImagemin from "vite-plugin-imagemin";
 
 const config: (configEnv: ConfigEnv) => UserConfig = ({ command }) => ({
   base: command === "serve" ? "" : "/dist/",
@@ -28,27 +26,6 @@ const config: (configEnv: ConfigEnv) => UserConfig = ({ command }) => ({
       targets: ["defaults", "not IE 11"],
     }),
     FullReload(["templates/**/*"]),
-    viteImagemin({
-      gifsicle: {
-        interlaced: true,
-      },
-      optipng: {
-        optimizationLevel: 5,
-      },
-      webp: {
-        quality: 85,
-      },
-      mozjpeg: {
-        quality: 85,
-        progressive: true,
-        arithmetic: false,
-      },
-      pngquant: {
-        quality: [0.75, 0.9],
-        speed: 4,
-      },
-      svgo: false,
-    }),
   ],
 });
 
