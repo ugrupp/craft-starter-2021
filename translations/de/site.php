a258ebab07244f3d6872320b6bdaf9eb613b740c
Urs Grupp <urs@21sieben.de>
2022-04-13 23:59:03 +0200

    refactor: use double quotes

--- translations/de/site.php
+++ translations/de/site.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-  'Jump to Content' => 'Zum Inhalt springen',
-  'Jump to Navigation' => 'Zum Menü springen',
+  "Jump to Content" => "Zum Inhalt springen",
+  "Jump to Navigation" => "Zum Menü springen",
 ];
